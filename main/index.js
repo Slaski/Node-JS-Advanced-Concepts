6be513d6c150901a85c854a50e416610aa649973
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-03-25 12:26:16 -0300

    chore: Removed the cluster configuration and installed the pm2 module to manage the cluster.

--- main/index.js
+++ main/index.js
@@ -1,37 +1,16 @@
-process.env.UV_THREADPOOL_SIZE = 1;
+// I'm a child, I'm going to act like a server and do nothing else
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-const cluster = require('cluster');
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send(`Hello from worker ${process.pid}`);
+    });        
+});
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-    // Cause index.js to be executed again but in child mode
-    console.log(`Master ${process.pid} is running`)
+app.get('/fast', (req, res) => {
+    res.send(`This is fast from worker ${process.pid}`);
+});
 
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-    })
-} else {
-    // I'm a child, I'm going to act like a server and do nothing else
-    const express = require('express');
-    const crypto = require('crypto');
-    const app = express();
-
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send(`Hello from worker ${process.pid}`);
-        });        
-    });
-
-    app.get('/fast', (req, res) => {
-        res.send(`This is fast from worker ${process.pid}`);
-    });
-
-    app.listen(3000);
-
-    console.log(`Worker ${process.pid} started`);
-}
+app.listen(3000);
